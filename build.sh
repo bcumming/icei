6198874aa34ab300931e7bcaa281d743e5a79e43
bcumming <bcumming@cscs.ch>
2018-10-11 13:33:02 +0200

    build parallel on desktop

--- build.sh
+++ build.sh
@@ -17,29 +17,49 @@ base_path=`pwd`
 source clean.sh
 
 #
-# prologue: set up environment
+# Prologue
 #
 
-#source daint-mc.sh
-#source daint-gpu.sh
+# Set up the environment, load modules, etc
 
-# set CC/CXX
-# set arbor variables: arch, with_gpu, etc
+# These variables configure the build of Arbor, specifically which architecture to target
+
+# Set to ON if targeting NVIDIA GPU.
+# Arbor requires minimum CUDA 9
+arb_gpu=OFF
+
+# CPU architecture to target (still set this if targetting GPU)
+# This corresponds to the GNU equivalent -march argument, e.g.
+#   native, broadwell, haswell, skylake, knl, skylake-512
+arb_arch=native
+
+# Whether to use SIMD template library: set to ON if the target architecture supports
+# one of AVX, AVX2 or AVX512.
+arb_vectorize=ON
+
+# Set the MPI compiler wrappers
+# These should use either of
+#   * gcc >= 6.0
+#   * clang >= 4.0
+# Note that CUDA will restrict which gcc version can be used.
+
+export CC=`which mpicc`
+export CXX=`which mpicxx`
+
+# Construct the arguments that will be passed to Arbor's CMake.
+cmake_args="-DARB_WITH_MPI=ON -DARB_WITH_GPU=$arb_gpu -DARB_ARCH=$arb_arch -DARB_VECTORIZE=$arb_vectorize"
 
 #
 # download arbor from github
+# you might want to replace this with a tar-ball?
 #
 
 repo_path="$base_path/arbor"
 git_repo="https://github.com/arbor-sim/arbor.git"
 
-log="$base_path/build_log"
-rm -f "$log"
-
 msg "cloning repository from $git_repo"
-git clone "$git_repo" "$repo_path" --recursive #&>> "$log"
+git clone "$git_repo" "$repo_path" --recursive
 exit_on_error "cloning repository"
-#exit_on_error "see ${log}"
 
 #
 # configure build with CMake
@@ -47,9 +67,9 @@ exit_on_error "cloning repository"
 
 build_path="$base_path/build"
 mkdir -p "$build_path"
-msg "configure with CMake in $build_path"
+msg "configure with CMake: cmake $repo_path $cmake_args"
 cd "$build_path"
-cmake "$repo_path"
+cmake "$repo_path" $cmake_args
 exit_on_error "configuring"
 
 #
